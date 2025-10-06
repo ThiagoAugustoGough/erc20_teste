c499f8830d4c67d18960e4491ee4f10126395b2c
ThiagoAugustoGough <chillur2003@gmail.com>
2025-10-06 18:14:36 -0300

    primeira versao non-fixed-token-supply

--- erc-20.sol
+++ erc-20.sol
@@ -6,6 +6,9 @@ contract erc20 {
     mapping(address individuo => mapping(address endAprovado => uint256 valorAprovado))
         public aprovadoPorEndereco;
 
+    address public immutable i_contractOwner;
+    uint256 internal totalSupplyAggregate; // imagino que uint256 seja o suficiente
+
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(
         address indexed _owner,
@@ -13,10 +16,11 @@ contract erc20 {
         uint256 _value
     );
 
-    //teste
-
     constructor(uint256 valorInicial) {
-        saldosPorEndereco[msg.sender] = valorInicial;
+        i_contractOwner = msg.sender;
+        saldosPorEndereco[i_contractOwner] = valorInicial;
+        totalSupplyAggregate += valorInicial;
+        emit Transfer(address(0), msg.sender, valorInicial);
     }
 
     function name() public pure returns (string memory) {
@@ -31,12 +35,29 @@ contract erc20 {
         return 18;
     }
 
-    function totalSupply() public pure returns (uint256) {
-        return 100000e18;
+    function totalSupply() public view returns (uint256) {
+        return totalSupplyAggregate;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return saldosPorEndereco[_owner];
+    }
+
+    function mint(address _reciever, uint256 _value) external returns(bool success) {
+        require(msg.sender == i_contractOwner, "Apenas o dono do contrato pode");
+        saldosPorEndereco[_reciever] += _value;
+        totalSupplyAggregate += _value;
+        emit Transfer(address(0), _reciever, _value);
+        return true;
+    }
+
+    function burn(address _loser, uint256 _value) external returns(bool success) {
+        require(msg.sender == i_contractOwner, "Apenas o dono do contrato pode");
+        require(saldosPorEndereco[_loser] >= _value, "Saldo insuficiente");
+        saldosPorEndereco[_loser] -= _value;
+        totalSupplyAggregate -= _value;
+        emit Transfer(_loser, address(0), _value);
+        return true;
     }
 
     function transfer(
@@ -53,7 +74,7 @@ contract erc20 {
         uint256 _value
     ) public returns (bool success) {
         require(aprovadoPorEndereco[_from][msg.sender] >= _value);
-        transferHelper(_to, _from, _value);
+        transferHelper(_from, _to, _value);
         aprovadoPorEndereco[_from][msg.sender] -= _value;
         return true;
     }
